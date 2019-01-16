1dba05581b6a040bab556dda4b98f4b9416a8b20
William Hua <william@attente.ca>
2019-01-16 15:03:09 -0500

    use wasm_bindgen

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,11 @@
+extern crate wasm_bindgen;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
 pub struct S {
 }
 
+#[wasm_bindgen]
 impl S {
     pub fn f() -> Result<(), &'static str> {
         Err("error")
